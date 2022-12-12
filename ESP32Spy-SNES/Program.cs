7be96f3c004482186eab23dacca42d38803c6a23
k.meerts198912 <55886806+KM198912@users.noreply.github.com>
2022-12-12 02:34:39 +0100

    More Error Handling

--- ESP32Spy-SNES/Program.cs
+++ ESP32Spy-SNES/Program.cs
@@ -3,7 +3,7 @@ using System.Threading;
 using System;
 using Nini.Config;
 using System.Linq;
-
+using System.IO;
 namespace SESP32SpySNES
 {
     class Program
@@ -12,17 +12,33 @@ namespace SESP32SpySNES
         static IConfigSource _source;
         public static void Main()
         {
-             _source = new IniConfigSource("ESP32Spy.ini");
-            string com_port = _source.Configs["Config"].Get("COM_PORT");
-            int baud = _source.Configs["Config"].GetInt("BaudRate");
-            bool portExists = SerialPort.GetPortNames().Any(x => x == com_port);
+            string dir = Directory.GetCurrentDirectory();
+            string file = "ESP32Spy.ini";
+            bool portExists = false;
+            string com_port = "";
+            int baud = 0;
+            if (@File.Exists(dir + @"\" + file))
+            {
+                _source = new IniConfigSource(file);
+
+                com_port = _source.Configs["Config"].Get("COM_PORT");
+                baud = _source.Configs["Config"].GetInt("BaudRate");
+               portExists = SerialPort.GetPortNames().Any(x => x == com_port);
+            }
+            else
+            {
+                Console.ForegroundColor = ConsoleColor.Red;
+                Console.WriteLine(file + " not found in " + dir);
+                Console.WriteLine("Did you forget to Copy it?");
+                while(true) { }
+            }
             if (portExists)
             {
                 _serialPort = new SerialPort();
             _serialPort.PortName =com_port;
             _serialPort.BaudRate = baud;
             _serialPort.Open();
-            Console.WriteLine("ESP32 SNES Reader");
+            Console.WriteLine("ESP32 SNES Reader v0.2");
             Console.WriteLine("Com Port: " + com_port + " Opened");
             Console.WriteLine("Baudrate: " + baud.ToString());
             while (true)
